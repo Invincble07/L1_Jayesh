4fc8a92a73013f7d5ed72bc6ed0ce134fbd0f554
Invincble07 <jfating0@gmail.com>
2023-06-18 20:48:52 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(
+    int num1, num2, number;
+    printf("Please Enter The First Number:"); 
+    scanf("%d", &num1);
+    printf("Please Enter The Second Number:");
+    scanf("%d", &num2);
+    if (num1 > num2)
+    printf("%d number is bigger",num1);
+    if(num2<num1)
+    printf("%d number is bigger",num2);
+    else
+    printf("The numbers are equal");
+    return 0;
+)
